2b331491b16cf30f4a28531fc837515ef0e17e68
Sanjaybhattwebkul <snajay.bhatt371@webkul.in>
2022-09-29 13:32:14 +0530

    TestNg annotation helper method implemented

--- core/src/main/java/Velocity/Customerlogin.java
+++ core/src/main/java/Velocity/Customerlogin.java
@@ -1,30 +1,42 @@
 package Velocity;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class Customerlogin extends bagissto.core.testCases{
 	
+	protected static WebDriver driver= new ChromeDriver();
+	 
 	@BeforeClass
-	public static void openBrowser() {
+	public static void configureBrowser() {
 		configBrowserExe("/chromedriver"); 		
 	}
 	
-	@Test
+	@Parameters({ "URL" })
+	@Test(priority = 0)
+	public void openBrowser(String URL) {
+		openBrowser(driver,URL);	// URL from testNg.xml file
+	}
+	
+	 @Test(dependsOnMethods={"openBrowser", "waite"})  // First run the wait() method then this
 	public static void Login() throws InterruptedException {
 			
 		String[] loginCredentials= {"tom@example.com","tom123"};		
 		String[] loginLocators = {"div[class*='welcome-content']","//input[@name='email']","//input[@name='password']"};	
-		WebDriver driver = new ChromeDriver();		
-		openBrowser(driver,"http://192.168.15.237/sanjay-bagisto/public/");	
-		Thread.sleep(2000);		
 		customerLogin(driver,loginLocators,loginCredentials); // customer Login	
 		
 	}
 	
 	@AfterMethod
-	public static void waite() throws InterruptedException {
+	public static void testMethod(){
+		System.out.println("I will run after every method");
+	}
+	
+	@Test
+	public static void waite() throws InterruptedException{
 		Thread.sleep(2000);
 	}
 
