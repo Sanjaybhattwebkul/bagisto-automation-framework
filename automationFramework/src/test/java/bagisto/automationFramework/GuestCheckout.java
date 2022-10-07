765b72bc4a6615b12b60c705529ed11ba8f59020
Sanjaybhattwebkul <snajay.bhatt371@webkul.in>
2022-10-07 18:49:39 +0530

    Code refector

--- automationFramework/src/test/java/bagisto/automationFramework/GuestCheckout.java
+++ automationFramework/src/test/java/bagisto/automationFramework/GuestCheckout.java
@@ -1,10 +1,13 @@
 package bagisto.automationFramework;
 import java.io.IOException;
+
+import org.testng.annotations.Test;
 import bagistoTestComponents.baseTest;
 import velocity.pageobjects.ProductListing;
 
 public class GuestCheckout extends baseTest {
 	
+	@Test
 	public void placeOrder() throws IOException, InterruptedException {
 		String productName = "Sunglasses";			
 		ProductListing ProductListingObj = launcVelocity();
