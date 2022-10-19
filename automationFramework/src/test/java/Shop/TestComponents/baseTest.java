417f60b877934f5472b0a1a4ed97e58f7e61a4c9
Sanjaybhattwebkul <snajay.bhatt371@webkul.in>
2022-10-19 14:34:16 +0530

    TestCses using Maven and TestNG profiling

--- automationFramework/src/test/java/Shop/TestComponents/baseTest.java
+++ automationFramework/src/test/java/Shop/TestComponents/baseTest.java
@@ -1,4 +1,5 @@
 package Shop.TestComponents;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,6 +14,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -23,68 +25,77 @@ import velocity.pageobjects.ProductListing;
 
 public class baseTest {
 	public WebDriver driver;
+
 	public WebDriver initlizeBrowser() throws IOException {
-		
+
 		// Get GlobalData using Properties class
-		Properties prop = new Properties(); // create Properties() object for get GlobalData.properties file 
-		FileInputStream files = new FileInputStream("/home/users/sanjay.bhatt/www/html/Bagisto-Automation/automationFramework/src/main/java/resources/GlobalData.properties");
+		Properties prop = new Properties(); // create Properties() object for get GlobalData.properties file
+		FileInputStream files = new FileInputStream(
+				"/home/users/sanjay.bhatt/www/html/Bagisto-Automation/automationFramework/src/main/java/resources/GlobalData.properties");
 		prop.load(files); // load the GlobalData.properties file.
 		String browserName = prop.getProperty("browser"); // get the browser name from GlobalData.properties file
-		
-		if(browserName.equalsIgnoreCase("chrome")) {
+
+		if (browserName.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-			System.out.println("BROWSER NAME="+browserName);
-		} else if(browserName.equalsIgnoreCase("fireFox")) {
-				// FireFox 	
-		}  else if(browserName.equalsIgnoreCase("edge")) {
-			//edge browser 
+			System.out.println("BROWSER NAME=" + browserName);
+		} else if (browserName.equalsIgnoreCase("fireFox")) {
+			// FireFox
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			// edge browser
 		} else {
-			System.out.println("BROWSER NAME="+browserName);
-			
+			System.out.println("BROWSER NAME=" + browserName);
+
 		}
-		
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		driver.manage().window().maximize();
 		return driver;
 	}
-	
-	
+
 	public ProductListing launcVelocity() throws IOException {
-		
+
 		driver = initlizeBrowser();
-		goToVelocityShop();  // open browser/site
+		goToVelocityShop(); // open browser/site
 		ProductListing ProductListing = new ProductListing(driver);
-		return ProductListing; // LandingPage object ko is liye return kiya kyuki loginApplication() function bhi call  ho ra hai.
+		return ProductListing; // LandingPage object ko is liye return kiya kyuki loginApplication() function
+								// bhi call ho ra hai.
 	}
-	
+
 	public void goToVelocityShop() {
 		driver.get("http://192.168.15.237/bagisto4.5/public/");
-		//scrollDown(driver);
+		// scrollDown(driver);
 	}
-	
+
 	@AfterMethod
 	public void closeBrowser() {
 		driver.close();
 	}
-	
-	// This function will get data from JSON file and return the data in from of HashMap 
+
+	// This function will get data from JSON file and return the data in from of
+	// HashMap
 	public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException {
-		//read json file  to string
-		String jsonContent = 	FileUtils.readFileToString(new File(filePath),StandardCharsets.UTF_8);
-		//String to HashMap- using ackson-databind
+		// read json file to string
+		String jsonContent = FileUtils.readFileToString(new File(filePath), StandardCharsets.UTF_8);
+		// String to HashMap- using ackson-databind
 		ObjectMapper mapper = new ObjectMapper();
-		 List<HashMap<String, String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>() {
-	      });
-		 return data;
-		//{map, map}
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent,
+				new TypeReference<List<HashMap<String, String>>>() {
+				});
+		return data;
+		// {map, map}
 	}
-	
-	public String getScreenshot(String testClassNmae,WebDriver driver) throws IOException {
-		TakesScreenshot ss = (TakesScreenshot)driver;
+
+	public String getScreenshot(String testClassNmae, WebDriver driver) throws IOException {
+		TakesScreenshot ss = (TakesScreenshot) driver;
 		File source = ss.getScreenshotAs(OutputType.FILE);
-		File File = new File(System.getProperty("user.dir")+"//Reports//"+testClassNmae+".png");
-		FileUtils.copyFile(source,File);
-		return System.getProperty("user.dir")+"//Reports//"+testClassNmae+".png";
+		File File = new File(System.getProperty("user.dir") + "//Reports//" + testClassNmae + ".png");
+		FileUtils.copyFile(source, File);
+		return System.getProperty("user.dir") + "//Reports//" + testClassNmae + ".png";
+	}
+
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+
 	}
 }
