b5072b1ad476c70f40a78e9822b6576f6e2a9c85
Sanjaybhattwebkul <snajay.bhatt371@webkul.in>
2022-10-31 18:52:45 +0530

    Code refector

--- programming/src/String/countWord.java
+++ programming/src/String/countWord.java
@@ -5,6 +5,14 @@ public class countWord {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		String[] stringArr = null;
+		String String = "the test string is the string the";
+		stringArr = String.split(" ");
+		for (int i = 0; i< stringArr.length; i++) {
+			System.out.println(stringArr[i]);
+		}
+		
+		
 		/*$String = "the test string is the string the";
 
 		$arr = explode(' ',$String);
