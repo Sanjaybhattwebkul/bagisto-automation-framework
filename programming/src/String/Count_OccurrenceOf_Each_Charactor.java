f829f134dca63ef847a0fc6c1ba60d764d8df8a6
Sanjaybhattwebkul <snajay.bhatt371@webkul.in>
2022-11-02 15:53:05 +0530

    COde refector

--- programming/src/String/Count_OccurrenceOf_Each_Charactor.java
+++ programming/src/String/Count_OccurrenceOf_Each_Charactor.java
@@ -8,7 +8,7 @@ public class Count_OccurrenceOf_Each_Charactor {
 	public static void main(String[] args) {
 		Scanner str= new Scanner(System.in);
 		System.out.println("Enter any String");
-		String inputString = str.next().replaceAll("\\s","");
+		String inputString = str.nextLine().replaceAll("\\s","");
 		
 		// Creating a HashMap containing char
         // as a key and occurrences as  a value
