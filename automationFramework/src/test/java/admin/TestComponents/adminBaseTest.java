19b11aee830d2b6b8f6c45c4cc70a977331c53e3
sanjay bhatt <sanjay.bhatt371@webkul.com>
2022-11-29 12:33:14 +0530

    File upload-co,lete

--- automationFramework/src/test/java/admin/TestComponents/adminBaseTest.java
+++ automationFramework/src/test/java/admin/TestComponents/adminBaseTest.java
@@ -1,5 +1,9 @@
 package admin.TestComponents;
 
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -93,6 +97,40 @@ public class adminBaseTest {
 		return System.getProperty("user.dir") + "//Reports//" + testClassNmae + ".png";
 	}
 
+	/**
+	 * 
+	 * This method will set any parameter string to the system's clipboard.
+	 * 
+	 */
+
+	public static void setClipboardData(String string) {
+		//StringSelection is a class that can be used for copy and paste operations.
+		StringSelection stringSelection = new StringSelection(string);
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+
+	}
+
+	public static void uploadFile(String fileLocation) {
+
+		try {
+
+			// Setting clipboard with file location
+			setClipboardData(fileLocation);
+			// native key strokes for CTRL, V and ENTER keys
+			Robot robot = new Robot();
+			robot.keyPress(KeyEvent.VK_CONTROL);
+			robot.keyPress(KeyEvent.VK_V);
+			robot.keyRelease(KeyEvent.VK_V);
+			robot.keyRelease(KeyEvent.VK_CONTROL);
+			robot.keyPress(KeyEvent.VK_ENTER);
+			robot.keyRelease(KeyEvent.VK_ENTER);
+
+		} catch (Exception exp) {
+			exp.printStackTrace();
+
+		}
+
+	}
 	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
 		
--- automationFramework/src/test/java/admin/Tests/UploadProductsImage.java
+++ automationFramework/src/test/java/admin/Tests/UploadProductsImage.java
@@ -1,24 +1,20 @@
 package admin.Tests;
 
-import java.awt.Robot;
-import java.awt.Toolkit;
-import java.awt.datatransfer.StringSelection;
-import java.awt.event.KeyEvent;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import admin.TestComponents.adminBaseTest;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class UploadProductsImage {
+public class UploadProductsImage extends adminBaseTest{
 
 	String URL = "https://www.ilovepdf.com/pdf_to_jpg";
 	
 	@Test
 	public void testUpload() throws InterruptedException {
-
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get(URL);
@@ -27,40 +23,4 @@ public class UploadProductsImage {
 		uploadFile("//home//users//sanjay.bhatt//Pictures//Abhay_Resume.pdf");
 		Thread.sleep(2000);
 	}
-
-	/**
-	 * 
-	 * This method will set any parameter string to the system's clipboard.
-	 * 
-	 */
-
-	public static void setClipboardData(String string) {
-		//StringSelection is a class that can be used for copy and paste operations.
-		StringSelection stringSelection = new StringSelection(string);
-		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
-
-	}
-
-	public static void uploadFile(String fileLocation) {
-
-		try {
-
-			// Setting clipboard with file location
-			setClipboardData(fileLocation);
-			// native key strokes for CTRL, V and ENTER keys
-			Robot robot = new Robot();
-			robot.keyPress(KeyEvent.VK_CONTROL);
-			robot.keyPress(KeyEvent.VK_V);
-			robot.keyRelease(KeyEvent.VK_V);
-			robot.keyRelease(KeyEvent.VK_CONTROL);
-			robot.keyPress(KeyEvent.VK_ENTER);
-			robot.keyRelease(KeyEvent.VK_ENTER);
-
-		} catch (Exception exp) {
-			exp.printStackTrace();
-
-		}
-
-	}
-
 }
