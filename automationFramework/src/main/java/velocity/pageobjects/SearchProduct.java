3be2ff58979a9eb7e6274b93cac27e8f98c173c7
sanjay bhatt <sanjay.bhatt371@webkul.com>
2022-12-07 19:43:26 +0530

    Price verification test complete

--- automationFramework/src/main/java/velocity/pageobjects/SearchProduct.java
+++ automationFramework/src/main/java/velocity/pageobjects/SearchProduct.java
@@ -1,10 +1,14 @@
 package velocity.pageobjects;
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
 import helpers.getProductsPrice;
 
 public class SearchProduct extends getProductsPrice{
@@ -22,9 +26,17 @@ public class SearchProduct extends getProductsPrice{
 	@FindBy(xpath="//span[@class='special-price']")
 	WebElement productsPrice;
 	
+	@FindBy(css="div[class*='card grid-card']")
+	WebElement productCard;
+	
+	
+	
 	public double searchProduct(String productName) {
 		searchBox.sendKeys(productName,Keys.ENTER);
-		return getActualPrice(productsPrice.getText());
+		boolean isproductFound = productCard.isDisplayed();
+		if(isproductFound) {
+			return getActualPrice(productsPrice.getText());
+		}
+		return (Double) null;
 	}
-
 }
--- automationFramework/src/test/java/common/VerifyProductPriceTest.java
+++ automationFramework/src/test/java/common/VerifyProductPriceTest.java
@@ -3,6 +3,7 @@ package common;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import baseComponent.BaseTest;
 import velocity.pageobjects.SearchProduct;
@@ -10,16 +11,19 @@ import velocity.pageobjects.SearchProduct;
 public class VerifyProductPriceTest extends BaseTest {
 	
 	@Test(dependsOnGroups={"AdminLoginTest.login"})
-	public void verifyPrice() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
+	public void verifyPrice() throws InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {		
 		PricePageObject priceObj = new PricePageObject(driver);			
 		List<String> price =    priceObj.getPriceFromBackend();		
-		System.out.println(price.get(0));
+		String backendPrice = price.get(0);
+		double expectedPrice = priceObj.getActualPrice(backendPrice);
 		SearchProduct ob = priceObj.visitShop();
 	    ArrayList<String> allTabs = new ArrayList<String>(driver.getWindowHandles()); // hold all window handles in array list
 	    driver.switchTo().window(allTabs.get(1));  //switch to new tab
 	    System.out.println("Page title of new tab: " + driver.getTitle());	      
-	    ob.searchProduct("Product-By Selnium Automation");
-		
-		
+	    double  actualPrice =  ob.searchProduct("Product-By Selnium Automation");
+	    System.out.println("Actual Price is : "+ expectedPrice);
+	    System.out.println("Actual Price is : "+ actualPrice);
+	    Assert.assertEquals(expectedPrice,actualPrice);
+	    System.out.println("Test Casse Passed");	 	
 	}
 }
