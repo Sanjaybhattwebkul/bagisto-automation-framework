3fdd084638b5f74bd9b42dadea2ad2f3502e5799
sanjay bhatt <sanjay.bhatt371@webkul.com>
2022-12-07 19:19:41 +0530

    getWindowHandles implemented

--- automationFramework/src/test/java/common/VerifyProductPriceTest.java
+++ automationFramework/src/test/java/common/VerifyProductPriceTest.java
@@ -1,6 +1,7 @@
 package common;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import org.testng.annotations.Test;
 import baseComponent.BaseTest;
@@ -14,6 +15,10 @@ public class VerifyProductPriceTest extends BaseTest {
 		List<String> price =    priceObj.getPriceFromBackend();		
 		System.out.println(price.get(0));
 		SearchProduct ob = priceObj.visitShop();
+	    ArrayList<String> allTabs = new ArrayList<String>(driver.getWindowHandles()); // hold all window handles in array list
+	    driver.switchTo().window(allTabs.get(1));  //switch to new tab
+	    System.out.println("Page title of new tab: " + driver.getTitle());	      
+	    ob.searchProduct("Product-By Selnium Automation");
 		
 		
 	}
