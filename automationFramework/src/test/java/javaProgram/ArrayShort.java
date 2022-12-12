8e857eae4e845250ca8cf16fdd741bbd05c07c16
sanjay bhatt <sanjay.bhatt371@webkul.com>
2022-12-12 16:00:19 +0530

    PROGRAMES ADDED

--- automationFramework/src/test/java/javaProgram/ArrayShort.java
+++ automationFramework/src/test/java/javaProgram/ArrayShort.java
@@ -7,35 +7,31 @@ public class ArrayShort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] array = {3,2,1,4,5};
-		sortTO(array,"ASC");
-		//sortTO(array,"DESC");
+		//sortTO(array,"ASC");
+		sortTO(array,"DESC");
 	}
 	
 	public static void sortTO(int[] array,String Type) {
 		int i,j;
-		int temp;
-		if(Type=="ASC") {
-			for (i=0; i<array.length;i++) {
-				for(j=0;j<array.length-1;j++) {
+		int temp;	
+		for (i=0; i<array.length;i++) {
+			for(j=0;j<array.length-1;j++) {
+				if(Type =="DESC") {
 					if(array[i] > array[j]) {
 						temp =array[i];
 						array[i] =array[j];
-						array[j]=temp;
+						array[j]=temp;				
 					}
-				}				
-			}
-			//System.out.print(Arrays.toString(array));
-		} else if(Type=="DESC"){
-			for (i=0; i<array.length;i++) {
-				for(j=0;j<array.length-1;j++) {
+				} else {
 					if(array[i] < array[j]) {
 						temp =array[i];
 						array[i] =array[j];
 						array[j]=temp;
 					}
-				}				
-			}
+				}					
+			}				
 		}
+		
 		System.out.print(Arrays.toString(array));
 	}
 
