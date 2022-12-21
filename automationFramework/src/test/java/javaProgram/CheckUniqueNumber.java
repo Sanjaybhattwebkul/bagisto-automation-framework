750ac694cec3f33f55a0534b99d4fae422180d7b
sanjay bhatt <sanjay.bhatt371@webkul.com>
2022-12-21 13:24:17 +0530

    Unique number

--- automationFramework/src/test/java/javaProgram/CheckUniqueNumber.java
+++ automationFramework/src/test/java/javaProgram/CheckUniqueNumber.java
@@ -5,11 +5,32 @@ import java.util.Scanner;
 public class CheckUniqueNumber {
 
 	public static void main(String[] args) {
-		//Scanner sc= new Scanner(System.in);
-		//System.out.println("Enter any number");
-		//int inputNumber = sc.nextInt();
-		
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Any Number");
+		int inputNumber = sc.nextInt();
+		if(checkNumber(inputNumber)) {
+			
+			System.out.println(inputNumber +" is unique Number");
+		} else {
+			System.out.println(inputNumber +" is not unique Number");
+		}
+	}
+	
+	public static boolean checkNumber(int inputNumber) {
+		boolean flag =true;		
+		String number = Integer.toString(inputNumber);
+		for(int i=0; i<number.length();i++) {
+			for(int j=i+1; j<number.length(); j++) {
+				if(number.charAt(i)== number.charAt(j)) {
+					flag =false;
+					break;
+				}
+			}
+			if(flag) {
+				break;
+			}
+		}
+		return flag;
 	}
 
 }
