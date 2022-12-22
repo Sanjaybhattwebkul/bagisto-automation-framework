e9393ff91057bea580ba950ab98511cb6cd2183d
sanjay bhatt <sanjay.bhatt371@webkul.com>
2022-12-22 19:11:04 +0530

    Verify the button color

--- automationFramework/src/test/java/Shop/Tests/test.java
+++ automationFramework/src/test/java/Shop/Tests/test.java
@@ -3,6 +3,12 @@ package Shop.Tests;
 import java.awt.AWTException;
 import java.io.IOException;
 import java.sql.SQLException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.Color;
 import org.testng.annotations.Test;
 import baseComponent.BaseTest;
 import helpers.productsActions;
@@ -11,12 +17,22 @@ import helpers.productsActions;
 public class test extends BaseTest{
 	@Test
 	public void testCase() throws IOException, InterruptedException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException, AWTException {
-		//JavascriptExecutor js = (JavascriptExecutor) driver;
-		//js.executeScript("window.scrollBy(0,400");
-		launcVelocity();		
-		productsActions ProductActions =  new productsActions(driver);
-		ProductActions.addProductsTo("COMPARE",1);
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/intl/en-GB/gmail/about/");
+		driver.manage().window().maximize();
+		WebElement button = driver.findElement(By.xpath("//a[contains(@class,'button')][3]"));
+		String colorInRGB = button.getCssValue("color");
+		String colorInHex = Color.fromString(colorInRGB).asHex();
 		
+		if(checkColor(colorInHex)) {
+			System.out.println("Button is green");
+		}else {
+			System.out.println("Button is Not green");
+		}		
+	}
+	
+	public boolean checkColor(String checkFor) {
+		return (checkFor=="#00FF00" ? true : false );
 	}
 
 	
