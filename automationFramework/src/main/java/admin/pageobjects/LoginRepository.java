d9950d2e0131449c4528b0c5deb132d2e18f38bd
Sanjaybhattwebkul <sanjay.bhatt371@webkul.in>
2023-02-20 16:46:18 +0530

    Admin login locators changes

--- automationFramework/src/main/java/admin/pageobjects/LoginRepository.java
+++ automationFramework/src/main/java/admin/pageobjects/LoginRepository.java
@@ -1,7 +1,9 @@
 package admin.pageobjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 import baseComponents.BaseRepository;
@@ -14,15 +16,15 @@ public class LoginRepository extends BaseRepository{
 		PageFactory.initElements(driver, this); //PageFactory TestNG ka part h is se annotation use kr skte h.
 	}
 	
-	@FindBy(id="email")
+	@FindBy(how = How.CSS, using = "#email") 
 	WebElement email;
 	
-	@FindBy(id="password")
+	@FindAll(@FindBy(how = How.CSS, using = "#password"))
 	WebElement Password;
 	
 	@FindBy(css="button[class='btn btn-xl btn-primary']")
 	WebElement loginButton;
-		
+	
 	public void adminLogin(String adminEmail, String adminPassword) {				
 		email.sendKeys(adminEmail);
 		Password.sendKeys(adminPassword);
